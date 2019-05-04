eeacb68caac5361477ad4396a97e417a29d083a7
jesus robles <jdrobles100704@gmail.com>
2019-05-04 15:20:46 -0400

    ok

--- pokemon-starter/scripts.js
+++ pokemon-starter/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+	$("#button1").click(getpokemon);
 
+	function getpokemon(){
+		let id = Math.floor(Math.random()*800);
+		$.get('https://pokeapi.co/api/v2/pokemon/${id}/',function(response){
+			console.log(response);
+		}
+			)
+		}
+	
 })
